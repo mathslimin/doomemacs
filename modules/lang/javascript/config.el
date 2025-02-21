39fee51c42e63d4a238fd15fd6c8e2a5c6a66bf2
limin100 <limin100@huawei.com>
2025-02-22 01:20:42 +0200

    change js tab from 2 to 4

--- modules/lang/javascript/config.el
+++ modules/lang/javascript/config.el
@@ -56,7 +56,7 @@
 
   (setq js-chain-indent t
         ;; These have become standard in the JS community
-        js2-basic-offset 2
+        js2-basic-offset 4
         ;; Don't mishighlight shebang lines
         js2-skip-preprocessor-directives t
         ;; let flycheck handle this
